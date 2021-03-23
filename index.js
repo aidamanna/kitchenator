86a13ee8bdcefdc4801db725878806a6950aedc6
Aida Manna <aidamanna@gmail.com>
2021-03-23 14:10:16 +0100

    add page container

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("it works!");
+import * as menuRespository from './menuRepository';
+
+const mainElement = document.querySelector('.main-wrapper');
+
+
+const menuItemsElement = menuRespository.get().map( dailyMenu  => 
+    `<li>
+    <div class="weekly-meny-day">${dailyMenu.day}<div>
+    <div class="weekly-menu-meal">Lunch: 
+        <a href="${dailyMenu.lunch.link}">${dailyMenu.lunch.title}</a>
+    </div>
+    <div class="weekly-menu-meal">Dinner: 
+        <a href="${dailyMenu.dinner.link}">${dailyMenu.dinner.title}</a>
+    </div>
+    </li>`).join('');
+
+
+const menuListElement = document.createElement('ul');
+menuListElement.classList.add("weekly-menu-list")
+menuListElement.innerHTML = menuItemsElement;
+
+
+console.log(menuListElement);
+
+mainElement.appendChild(menuListElement);
---
+++ menuRepository.js
@@ -0,0 +1,83 @@
+const menu = [
+    {
+        "day": "22032021",
+        "lunch": {
+            "title": "Lentejas",
+            "link": ""
+        },
+        "dinner": {
+            "title": "Estofado de berenjena",
+            "link": ""
+        }
+    },
+    {
+        "day": "23032021",
+        "lunch": {
+            "title": "Quiche veggie",
+            "link": ""
+        },
+        "dinner": {
+            "title": "Crema de calabaza con curry",
+            "link": ""
+        }
+    },
+    {
+        "day": "24032021",
+        "lunch": {
+            "title": "Curry verde Tailandes de pollo",
+            "link": ""
+        },
+        "dinner": {
+            "title": "Pescado",
+            "link": ""
+        }
+    },
+    {
+        "day": "25032021",
+        "lunch": {
+            "title": "Macarrones",
+            "link": ""
+        },
+        "dinner": {
+            "title": "Sopa",
+            "link": ""
+        }
+    },
+    {
+        "day": "26032021",
+        "lunch": {
+            "title": "Quiche veggie",
+            "link": ""
+        },
+        "dinner": {
+            "title": "Tortilla de patatas",
+            "link": ""
+        }
+    },
+    {
+        "day": "27032021",
+        "lunch": {
+            "title": "Quiche veggie",
+            "link": ""
+        },
+        "dinner": {
+            "title": "Crema de calabaza con curry",
+            "link": ""
+        }
+    },
+    {
+        "day": "28032021",
+        "lunch": {
+            "title": "Quiche veggie",
+            "link": ""
+        },
+        "dinner": {
+            "title": "Crema de calabaza con curry",
+            "link": ""
+        }
+    }
+]
+
+export function get() {
+    return menu;
+}
