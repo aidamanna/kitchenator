f785f60e2f1fff024a66309fa38bb9ee1b81424c
Aida Manna <aidamanna@gmail.com>
2021-03-26 21:55:41 +0100

    Assume aws role in script

--- infrastructure/kitchenator/apply.sh
+++ infrastructure/kitchenator/apply.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
 set -euxo pipefail
-
 cd "$(dirname "$0")"
-
 rm -rf ./.terraform
+
+ROLE_ARN="arn:aws:iam::${AWS_ACCOUNT_ID}:role/pipeline-role"
+
+ASSUMED_ROLE=$(
+    aws sts assume-role \
+        --role-arn ${ROLE_ARN} \
+        --role-session-name deploy-infrastructure
+)
+
+export AWS_SESSION_TOKEN=$(echo ${ASSUMED_ROLE} | jq -r '.Credentials.SessionToken')
+export AWS_ACCESS_KEY_ID=$(echo ${ASSUMED_ROLE} | jq -r '.Credentials.AccessKeyId')
+export AWS_SECRET_ACCESS_KEY=$(echo ${ASSUMED_ROLE} | jq -r '.Credentials.SecretAccessKey')
 
 terraform init
 terraform plan -out=kitchenator.plan
--- infrastructure/kitchenator/main.tf
+++ infrastructure/kitchenator/main.tf
@@ -9,8 +9,5 @@ terraform {
 }
 
 provider "aws" {
-  assume_role {
-    role_arn = "arn:aws:iam::${AWS_ACCOUNT_ID}:role/pipeline-role"
-  }
   region = local.region
 }
