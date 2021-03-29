95f116b93c61009915450c4147f42dfcfa534b81
Aida Manna <aida.mannaloyarte@thoughtworks.com>
2021-03-29 10:45:11 +0200

    Assume pipeline role in deploy script

--- infrastructure/kitchenator/deploy-website.sh
+++ infrastructure/kitchenator/deploy-website.sh
@@ -2,4 +2,16 @@
 
 set -euxo pipefail
 
+ROLE_ARN="arn:aws:iam::${AWS_ACCOUNT_ID}:role/pipeline-role"
+
+ASSUMED_ROLE=$(
+    aws sts assume-role \
+        --role-arn ${ROLE_ARN} \
+        --role-session-name deploy-infrastructure
+)
+
+export AWS_SESSION_TOKEN=$(echo ${ASSUMED_ROLE} | jq -r '.Credentials.SessionToken')
+export AWS_ACCESS_KEY_ID=$(echo ${ASSUMED_ROLE} | jq -r '.Credentials.AccessKeyId')
+export AWS_SECRET_ACCESS_KEY=$(echo ${ASSUMED_ROLE} | jq -r '.Credentials.SecretAccessKey')
+
 aws s3 sync ./dist s3://devicenator.com --delete
