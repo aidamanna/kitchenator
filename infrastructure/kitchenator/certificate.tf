04e7f91e151a42208b14b4a9675bf8099917a9bd
Aida Manna <aidamanna@gmail.com>
2021-04-27 15:49:17 +0200

    Fix terraform files

--- infrastructure/kitchenator/certificate.tf
+++ infrastructure/kitchenator/certificate.tf
@@ -16,7 +16,7 @@ resource "aws_acm_certificate" "certificate" {
 resource "aws_route53_record" "certificate_validation" {
   name = aws_acm_certificate.certificate.domain_validation_options[0].resource_record_name
   type = aws_acm_certificate.certificate.domain_validation_options[0].resource_record_type
-  zone_id = data.aws_route53_zone.devicenator.zone_id
+  zone_id = aws_route53_zone.kitchenator.zone_id
   records = [aws_acm_certificate.certificate.domain_validation_options[0].resource_record_value]
   ttl = 60
 }
--- infrastructure/kitchenator/dns.tf
+++ infrastructure/kitchenator/dns.tf
@@ -1,14 +1,14 @@
 resource "aws_route53_zone" "kitchenator" {
-  name = "devicenator.com"
+  name = local.domain_name
 }
 
 resource "aws_route53_record" "kitchenator" {
   zone_id = aws_route53_zone.kitchenator.zone_id
-  name    = "devicenator.com"
-  type    = "A"
+  name = local.domain_name
+  type = "A"
 
   alias {
-    name    = aws_s3_bucket.kitchenator.website_domain
+    name = aws_s3_bucket.kitchenator.website_domain
     zone_id = aws_s3_bucket.kitchenator.hosted_zone_id
     evaluate_target_health = false
   }
